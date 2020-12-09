9463626f41a84e8d6ae6ad03658ac88c689b99b8
mrblackx <63623649+rebl0x3r@users.noreply.github.com>
2020-12-09 08:43:55 +0100

    Fix Update 1.6 | Installer.sh
    - Added Z-Shell Burpy Command (1.0)
    - Added A Kill So That User Start Shell Again (1.1)
    - Fixed Killing XTERM Processes (1.2)
    - Fixed Burpy Command Not Found (view 1.1)
    - Fixed Package Installation Missing Sudo (1.3)

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
 # __author__     : @mrblackx
-# __version__    : v1.5
+# __version__    : v1.6
 # __description__: burpsuite pro setup
 # __support__    : https://t.me/burp_chat
 # __burpsuite__  : v2020.11.3
-# __changelog__  : Added Auto Pull-Update (GitHub)
+# __changelog__  : Added Z-Shell Support, Kill Shell For Restart, Fixed Errors (2)
 # 
 
 r="\e[31m"
@@ -41,73 +41,90 @@ update(){
 	echo -e "${g}[*] ${w}Checking if you up-to-date";run
 	sleep 0.5
 	if [[ "$up" == "Already up to date." ]]; then
-		echo -e "${g}[i] ${w}Already on the latest version :-)"
+		echo -e "${g}[i] ${w}Already on the latest version :-)${rs}"
 	elif [[ "$up" == "Already up-to-date." ]]; then
-		echo -e "${g}[i] ${w}Already on the latest version :-)"
+		echo -e "${g}[i] ${w}Already on the latest version :-)${rs}"
 	elif [[ "$up" == "Bereits aktuell." ]]; then
-		echo -e "${g}[i] ${w}Already on the latest version :-)"
+		echo -e "${g}[i] ${w}Already on the latest version :-)${rs}"
 	else
 		echo -e "${r}[!] ${w}Outdated, updating wait...."
 		git pull -q &>/dev/null
-		echo -e "${g}[*] ${w}Updated to the newest version :-)"
+		echo -e "${g}[*] ${w}Updated to the newest version :-)${rs}"
 	fi
 
 }
 
 main(){
-	echo -e "${bl}${b}[${g}*${b}] ${w}I will start Burpsuite, all you have to do is activate it manually.\nAfter activation, you can go back here and i will make the rest."
+	echo -e "${bl}${b}[${g}*${b}] ${w}I will start Burpsuite, all you have to do is activate it manually.\nAfter activation, you can go back here and i will make the rest.${rs}"
 	xterm -e java -javaagent:BurpSuiteLoader_v2020.11.3.jar -noverify -jar burpsuite_pro_v2020.11.3.jar &
 	xterm -e java -jar burploader-old.jar &
-	echo -ne "${bl}${b}[${g}*${b}] ${w}Hit enter if you have activated the burpsuite correctly> "
+	echo -ne "${bl}${b}[${g}*${b}] ${w}Hit enter if you have activated the burpsuite correctly> ${rs}"
 	read enter
-	ps aux | grep xterm | sed 's/   / /g' | cut -d\  -f2 | head -n2 | xargs kill -9 &>/dev/null
+#	ps aux | grep xterm | sed 's/   / /g' | cut -d\  -f2 | head -n2 | xargs kill -9 &>/dev/null
+#   if the command down below not work try the commented command as well
+	ps aux | grep xterm | sed 's/    / /g' | cut -d\  -f2 | head -n2 | xargs kill -9 &>/dev/null
 	ls *.burp &>/dev/null
 	if [ $? -eq 0 ]; then
-		echo -ne "${bl}${b}[${g}*${b}] ${w}Finishing setup"; run
-		laste=$(cat ~/.bashrc | tail -1)
+		echo -ne "${bl}${b}[${g}*${b}] ${w}Finishing setup${rs}"; run
+		laste=$(cat ~/.bashrc | tail -1 | awk '{print $2}')
+		laste_=$(cat ~/.zshrc | tail -1 | awk '{print $2}')
+		
 		path=$(pwd)
-		if [[ "${laste}" == "alias burpy='cd ${path}; java -javaagent:BurpSuiteLoader_v2020.11.3.jar -noverify -jar burpsuite_pro_v2020.11.3.jar&'" ]]; then
-			echo -e "\n${bl}${b}[${g}*${b}] ${w}Already setuped as ${g}burpy ${w}command."
+		
+		if [[ "${laste}" == "burpy='cd" ]]; then
+			echo -e "\n${bl}${b}[${g}*${b}] ${w}Already setuped as ${g}burpy ${w}command.${rs}"
 			fix_errors
 		else
-			echo -e "\n${bl}${b}[${g}*${b}] ${w}Adding ${g}burpy ${w}command."
+			echo -e "\n${bl}${b}[${g}*${b}] ${w}Adding ${g}burpy ${w}command.${rs}"
 			echo -e "alias burpy='cd ${path}; java -javaagent:BurpSuiteLoader_v2020.11.3.jar -noverify -jar burpsuite_pro_v2020.11.3.jar&'" >> ~/.bashrc
 			fix_errors
 		fi
-	elif [ $? -eq 2 ]; then
-		echo -e "${bl}${b}[${r}!${b}] ${w}You didn't followed the instructions, can't find burp project file!"
+
+		if [[ "${laste_}" == "burpy='cd" ]]; then
+			echo -e "\n${bl}${b}[${g}*${b}] ${w}Already setuped as ${g}burpy ${w}command.${rs}"
+			fix_errors
+		else
+			echo -e "\n${bl}${b}[${g}*${b}] ${w}Adding ${g}burpy ${w}command.${rs}"
+			echo -e "alias burpy='cd ${path}; java -javaagent:BurpSuiteLoader_v2020.11.3.jar -noverify -jar burpsuite_pro_v2020.11.3.jar&'" >> ~/.zshrc
+			fix_errors
+		fi
+	#elif [ $? -eq 2 ]; then
+	else
+		echo -e "${bl}${b}[${r}!${b}] ${w}You didn't followed the instructions, can't find burp project file!${rs}"
 		exit 1
+	
+
 	fi
 }
 
 file_check(){
 	i=0
 	if [ -f BurpSuiteLoader_v2020.11.3.jar ]; then
-		echo -e "${bl}${b}[${g}✓${b}] ${w}Found ${c}BurpSuiteLoader_v2020.11.3.jar"
+		echo -e "${bl}${b}[${g}✓${b}] ${w}Found ${c}BurpSuiteLoader_v2020.11.3.jar${rs}"
 		i=$(( $i + 1 ))
 	elif [ ! -f BurpSuiteLoader_v2020.11.3.jar ]; then
-		echo -e "${bl}${b}[${r}✗${b}] ${w}Not found ${c}BurpSuiteLoader_v2020.11.3.jar"
+		echo -e "${bl}${b}[${r}✗${b}] ${w}Not found ${c}BurpSuiteLoader_v2020.11.3.jar${rs}"
 		exit 1
 	fi
 	if [ -f burploader-old.jar ]; then
-		echo -e "${bl}${b}[${g}✓${b}] ${w}Found ${c}burploader-old.jar"
+		echo -e "${bl}${b}[${g}✓${b}] ${w}Found ${c}burploader-old.jar${rs}"
 		i=$(( $i + 1 ))
 	elif [ ! -f burploader-old.jar ]; then
-		echo -e "${bl}${b}[${r}✗${b}] ${w}Not found ${c}burploader-old.jar"
+		echo -e "${bl}${b}[${r}✗${b}] ${w}Not found ${c}burploader-old.jar${rs}"
 		exit 1
 	fi
 	if [ -f burpsuite_pro_v2020.11.3.jar ]; then
-		echo -e "${bl}${b}[${g}✓${b}] ${w}Found ${c}burpsuite_pro_v2020.11.3.jar"
+		echo -e "${bl}${b}[${g}✓${b}] ${w}Found ${c}burpsuite_pro_v2020.11.3.jar${rs}"
 		i=$(( $i + 1 ))
 	elif [ ! -f burpsuite_pro_v2020.11.3.jar ]; then
-		echo -e "${bl}${b}[${r}✗${b}] ${w}Not found ${c}burpsuite_pro_v2020.11.3.jar"
+		echo -e "${bl}${b}[${r}✗${b}] ${w}Not found ${c}burpsuite_pro_v2020.11.3.jar${rs}"
 		exit 1
 	fi
 
 	if [ "${i}" -eq 3 ]; then
 		main
 	else
-		echo -e "${bl}${b}[${r}✗${b}] ${w}Sorry, some files are missing and i can't continue!"
+		echo -e "${bl}${b}[${r}✗${b}] ${w}Sorry, some files are missing and i can't continue!${rs}"
 		exit 1
 	fi
 }
@@ -115,12 +132,12 @@ file_check(){
 getinfo(){
 	l="."
 	
-	cmd=$(apt list --installed ${item} | grep -o "installed\|installed" &>/dev/null)
 	packets=(openjdk-14-dbg openjdk-14-demo openjdk-14-doc openjdk-14-jdk openjdk-14-jdk-headless openjdk-14-jre openjdk-14-jre-zero openjdk-14-source)
+	cmd=$(sudo apt list --installed ${item} | grep -o "installed\|installed" &>/dev/null)
 	for item in ${packets[@]}
 	do
-		if ! ${cmd}; then
-			echo -ne "${bl}${b}[${r}!${b}] ${w}Package ${r}${item} ${w}not found.\nInstalling it for you"; run
+		if [ ! ${cmd} ]; then
+			echo -ne "${bl}${b}[${r}!${b}] ${w}Package ${r}${item} ${w}not found.\nInstalling it for you${rs}"; run
 			sudo apt install ${item} -y
 		fi
 	done
@@ -131,11 +148,12 @@ getinfo(){
 
 
 fix_errors(){
-	echo -e "\n${bl}${b}[${g}*${b}] ${w}Fixing common errors, please give root password if required."
+	echo -e "\n${bl}${b}[${g}*${b}] ${w}Fixing common errors, please give root password if required.${rs}"
 	sudo sysctl -w kernel.unprivileged_userns_clone=1
 	x=$(java --version | head -1 | cut -d\  -f2 | cut -d"." -f1)
 	if [[ "${x}" == "14" ]]; then
 		echo -e "${bl}${b}[${g}*${b}] ${w}Up-To-Date."
+		sleep 1; kill -9 $$
 	else
 		echo -e "${bl}${b}[${g}*${b}] ${w}Re-Fixing";run
 		getinfo
@@ -146,17 +164,17 @@ check(){
 	clear;banner
 	if [[ "${EUID}" == 0 ]]; then
 		update
-		echo -e "${bl}${b}[${r}!${b}] ${w}Warning!\n\nYou are running this script as ${r}root ${w}user.\nNormally this has ${r}no influence${w}, but please be careful and enter the super-user password manually."; sleep 0.5
+		echo -e "${bl}${b}[${r}!${b}] ${w}Warning!\n\nYou are running this script as ${r}root ${w}user.\nNormally this has ${r}no influence${w}, but please be careful and enter the super-user password manually.${rs}"; sleep 0.5
 		echo -ne "${bl}${b}[${y}?${b}] ${w}Do you want to start anyway?[${g}Y${w}/${r}N${w}]:${b} "
 		read t
 		if [[ "${t}" == "y" || "${t}" == "Y" ]]; then
 			xterm=$(which xterm)
 			if [[ "${xterm}" == "" ]]; then
-				echo -e "${bl}${b}[${r}!${b}] ${g}xterm ${w}is not installed yet, let me install.${y}"
+				echo -e "${bl}${b}[${r}!${b}] ${g}xterm ${w}is not installed yet, let me install.${rs}${y}"
 				sudo apt install xterm -y
 				getinfo
 			elif [[ "${xterm}" == "/usr/bin/xterm" ]]; then
-				echo -e "${bl}${b}[${g}*${b}] ${g}xterm ${w}found, continue";run
+				echo -e "${bl}${b}[${g}*${b}] ${g}xterm ${w}found, continue${rs}";run
 				getinfo
 			fi
 		elif [[ "${t}" == "n" || "${t}" == "N" ]]; then
@@ -164,14 +182,14 @@ check(){
 		fi
 	elif [[ "${ueuid}" == 1000 ]]; then
 		update
-		echo -e "${bl}${b}[${g}*${b}] ${w}Well you are running the script as common user, please wait."; sleep 0.5
+		echo -e "${bl}${b}[${g}*${b}] ${w}Well you are running the script as common user, please wait.${rs}"; sleep 0.5
 		xterm=$(which xterm)
 			if [[ "${xterm}" == "" ]]; then
-				echo -e "${bl}${b}[${r}!${b}] ${g}xterm ${w}is not installed yet, let me install.${y}"
+				echo -e "${bl}${b}[${r}!${b}] ${g}xterm ${w}is not installed yet, let me install.${rs}${y}"
 				sudo apt install xterm -y
 				getinfo
 			elif [[ "${xterm}" == "/usr/bin/xterm" ]]; then
-				echo -e "${bl}${b}[${g}*${b}] ${g}xterm ${w}found, continue";run
+				echo -e "${bl}${b}[${g}*${b}] ${g}xterm ${w}found, continue${rs}";run
 				getinfo
 			fi
 	fi
